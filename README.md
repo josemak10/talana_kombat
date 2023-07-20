b416601121006370467c52a0294d14381b952e93
Joshe Neira <josemak10@gmail.com>
2023-07-20 15:36:38 -0500

    add readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+TALANA KOMBAT
+
+Esta hecho en python y al ejecutar el archivo , se va a ejecutar uno de los ejemplos expuestos en el documento.
+
+Para ejecutar los otros, tan solo se debe comentar el activo
--- talana_kombat.py
+++ talana_kombat.py
@@ -123,6 +123,6 @@ def main(fight_string):
         print(f"{data[player2]['name']} gana")
 
 if __name__=="__main__":
-    #main('{"player1":{"movimientos":["D","DSD","S","DSD","SD"],"golpes":["K","P","","K","P"]},"player2": {"movimientos":["SA","SA","SA","ASA","SA"],"golpes":["K","","K","P","P"]}}')
+    main('{"player1":{"movimientos":["D","DSD","S","DSD","SD"],"golpes":["K","P","","K","P"]},"player2": {"movimientos":["SA","SA","SA","ASA","SA"],"golpes":["K","","K","P","P"]}}')
     #main('{"player1":{"movimientos":["SDD", "DSD", "SA", "DSD"],"golpes":["K", "P", "K", "P"]},"player2":{"movimientos":["DSD", "WSAW", "ASA", "", "ASA", "SA"],"golpes":["P", "K", "K", "K", "P", "k"]}}')
-    main('{"player1":{"movimientos":["DSD", "S"] ,"golpes":[ "P", ""]},"player2":{"movimientos":["", "ASA", "DA", "AAA", "", "SA"],"golpes":["P", "", "P", "K", "K", "K"]}} ')
+    #main('{"player1":{"movimientos":["DSD", "S"] ,"golpes":[ "P", ""]},"player2":{"movimientos":["", "ASA", "DA", "AAA", "", "SA"],"golpes":["P", "", "P", "K", "K", "K"]}} ')
